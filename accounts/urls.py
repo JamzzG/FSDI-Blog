df51bd1743a1dd117855cdeeb5659336335f88ab
James Grantham <granthamjames@yahoo.com>
2023-03-04 10:02:40 -0700

    Halfway through makeup class

---
+++ accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    SignUpView,
+    ChangePasswordView,
+    PasswordChangedView,
+    PasswordResetView
+)
+
+
+urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
+    # path("change_password_form/", ChangePasswordView.as_view(), name="change_password"),
+    # path("signup/", PasswordChangedView.as_view(), name="password_changed"),
+]
+
